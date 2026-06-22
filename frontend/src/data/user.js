2959d347a8ab6fd7ba36ca9c5b9b65bbc6aebb74
Muhammed Faheem <faheempurakkatu@gmail.com>
2026-06-22 18:07:26 +0530

    Added Movie Details Component

---
+++ frontend/src/data/user.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    id: 1,
+    name: 'Customer User',
+    email: 'customer@gmail.com',
+    password: '123456',
+    role: 'customer',
+  },
+  {
+    id: 2,
+    name: 'Admin User',
+    email: 'admin@gmail.com',
+    password: 'admin123',
+    role: 'admin',
+  },
+]
+
+export default users
---
+++ frontend/src/pages/MyBookings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyBookings = () => {
+  return (
+    <div className='flex justify-center align-middle m-10'>
+        <p className='text-3xl font-extrabold'>My Bookings Page</p>
+    </div>
+  )
+}
+
+export default MyBookings
