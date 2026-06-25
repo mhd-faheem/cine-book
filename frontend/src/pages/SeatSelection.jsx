cab8bbed48889d8f90f91e826babc61bc6e8e478
mhd-faheem <faheempurakkatu@gmail.com>
2026-06-25 14:40:54 +0530

    Add basic seat selection and ticket logic

--- frontend/src/pages/SeatSelection.jsx
+++ frontend/src/pages/SeatSelection.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import '../styles/SeatSelection.css'
 import Navbar from '../components/Navbar'
 
 const SeatSelection = () => {
+
+  const [selectedSeats, setSelectedSeats] = useState([])
+  let updatedSeats = [];
+  let tickets = 3;
+  let seatPrice = 150;
   
   // Extract id from URL
   let params = useParams();
@@ -34,11 +39,37 @@ const SeatSelection = () => {
           row: "A",
           number: 4,
           status: "available"
+        },
+        {
+          id: "A5",
+          row: "A",
+          number: 5,
+          status: "available"
         }
       ];
 
   function handleSeatSelect(seat){
     console.log(`seat clicked: ${seat.id}`);
+    console.log(selectedSeats.includes(seat.id))
+
+    // Unselect Seat if already selected
+    if(selectedSeats.includes(seat.id)){
+      let updatedSeats = selectedSeats.filter((selectedSeat) => {
+      return selectedSeat !== seat.id
+    })
+
+    setSelectedSeats(updatedSeats)
+    // Add selected seats to list
+    } else if (!selectedSeats.includes(seat.id) &&  selectedSeats.length<=tickets-1) {
+      let updatedSeats =  [...selectedSeats, seat.id]
+      setSelectedSeats(updatedSeats)
+    } else {
+      setSelectedSeats([])
+    }
+
+    console.log(selectedSeats)  
+
+  
   }
   
   // Return UI
@@ -50,8 +81,11 @@ const SeatSelection = () => {
 
       {/* Start Main Wrapper */}
       <div className='main-wrapper'>
-        <Link to={`/movies/${id}`}>&larr; Back</Link>
-        <p className='text-3xl mb-3'>Seat Booking</p>
+        <Link to={`/movies/${id}`} className='back-button'>&larr; Back</Link>
+        <div className='flex justify-between'>
+          <p className='text-3xl mb-3'>Movie Name: Showtime</p>
+          <p>Tickets: {tickets}</p>
+        </div>
         
         {/* Seats UI */}
         <div className='seat-ui flex flex-col justify-center align-bottom items-center gap-15'>
@@ -65,14 +99,48 @@ const SeatSelection = () => {
           {/* Seats */}
           <div className="seats flex gap-3">
             {
-              seats.map((seat) => 
-              <button onClick={() => handleSeatSelect(seat)} 
-              key={seat.id} 
-              className='p-4 border cursor-pointer'>
-                {seat.id}
-              </button>
-            )}
+              // Mapping each seat from seats array
+              seats.map((seat) => {
+              // Seat Styling variables
+              let seatBgColor = 'white'
+              let seatTextColor = 'black'
+              let seatCursor = 'pointer'
+
+              if (selectedSeats.includes(seat.id)) {
+                seatBgColor = 'green'
+                seatTextColor = 'white'
+              }
+
+              if (seat.status === "booked") {
+                seatBgColor = 'grey'
+                seatCursor = 'not-allowed'
+              }
+
+              return (
+                <button
+                  onClick={() => handleSeatSelect(seat)}
+                  disabled={seat.status === "booked"}
+                  key={seat.id}
+                  className='p-4 border cursor-pointer'
+                  style={{
+                    backgroundColor: seatBgColor,
+                    color: seatTextColor,
+                    cursor: seatCursor,
+                    fontSize: '20px',
+                  }}
+                >
+                  {seat.id}
+                </button>
+              )
+              })}
           </div>
+        </div>
+          {selectedSeats.length>0 && (
+            <div className='flex flex-col justify-center items-center mt-10'>
+              <p className='text-xl'>Selected Seats: {selectedSeats.join(', ')}</p>
+              <button>Pay &#8377;{selectedSeats.length*seatPrice}</button>
+            </div>
+          )}
         </div>
       </div>
     </div>
